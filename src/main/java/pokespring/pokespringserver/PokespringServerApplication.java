34b2a15cbbdb3058d783f9d21da1c32c903baba6
Bartosz Wasilew <barofficia@vp.pl>
2020-04-18 15:07:55 +0200

    Init pokespring server side

---
+++ src/main/java/pokespring/pokespringserver/PokespringServerApplication.java
@@ -0,0 +1,13 @@
+package pokespring.pokespringserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokespringServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokespringServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pokespring/pokespringserver/PokespringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pokespring.pokespringserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokespringServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
