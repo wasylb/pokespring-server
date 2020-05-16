c4ff77617948753a9fda413e9a78a1b134e789d3
Wasilew <bartosz.wasilew@accenture.com>
2020-05-16 15:18:51 +0200

    CORS rule (for /pokemons endpoint) implemented

--- src/main/java/pokespring/pokespringserver/controller/PokemonController.java
+++ src/main/java/pokespring/pokespringserver/controller/PokemonController.java
@@ -10,6 +10,7 @@ import pokespring.pokespringserver.repository.PokemonRepository;
 import javax.validation.Valid;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping(value = {"/pokemons"})
 public class PokemonController {
